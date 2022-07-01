ec7b3fe7f5de6fb9f3e28c22ce25cc7eaa251d11
Lalluviadel <inspiracion@yandex.ru>
2022-07-01 04:59:39 +0300

    - fixed info modal window closing by timeout;

--- static/js/site_actions.js
+++ static/js/site_actions.js
@@ -1,6 +1,6 @@
 /**
-  * Contains javascript scripts for asynchronous operation of site elements.
-  */
+ * Contains javascript scripts for asynchronous operation of site elements.
+ */
 
 window.addEventListener('load', (e) => {
 
@@ -8,7 +8,7 @@ window.addEventListener('load', (e) => {
      * Allows you to make a category available/unavailable at the click of a button
      * without reloading the entire page, while preserving the original pagination.
      * Works in the admin panel on the category list page.
-    */
+     */
     $('#update_cat_catcher').on('click', '#update_cat_btn', (e) => {
         let t_href = e.target;
         let checkBox = document.getElementById("option2");
@@ -38,7 +38,7 @@ window.addEventListener('load', (e) => {
      * Allows you to make a question available/unavailable at the click of a button
      * without reloading the entire page, while preserving the original pagination.
      * Works in the admin panel on the question list page.
-    */
+     */
     $('#update_que_catcher').on('click', '#update_que_btn', (e) => {
         let t_href = e.target;
         let checkBox = document.getElementById("option2");
@@ -71,7 +71,7 @@ window.addEventListener('load', (e) => {
      * Allows you to make a user active/inactive at the click of a button
      * without reloading the entire page, while preserving the original pagination.
      * Works in the admin panel on the user list page.
-    */
+     */
     $('#update_users_catcher').on('click', '#update_user_btn', (e) => {
         let t_href = e.target;
         let checkBox = document.getElementById("option2");
@@ -104,7 +104,7 @@ window.addEventListener('load', (e) => {
      * Allows you to give the user the rights of a superuser/deprive of these rights
      * at the touch of a button without reloading the entire page, while preserving the original pagination.
      * Works in the admin panel on the user list page.
-    */
+     */
     $('#update_post_catcher').on('click', '#update_post_btn', (e) => {
         let t_href = e.target;
         let checkBox = document.getElementById("option2");
@@ -137,7 +137,7 @@ window.addEventListener('load', (e) => {
      * Allows you to make a user active/inactive at the click of a button
      * without reloading the entire page, while preserving the original pagination.
      * Works in the admin panel on the users list page.
-    */
+     */
     $('#give_me_a_crown').on('click', '#give_me_a_crown_btn', (e) => {
         let t_href = e.target;
 
@@ -165,7 +165,7 @@ window.addEventListener('load', (e) => {
      * Works with any pictures except category pictures and pictures on the main
      * page of the site. When you click on the image, it will be opened in
      * a modular window and enlarged for the convenience of the user.
-    */
+     */
     $('div.thumbnail').click(function (e) {
         e.preventDefault();
         $('#image-modal .modal-body img').attr('src', $(this).find('img').attr('src'));
@@ -176,7 +176,7 @@ window.addEventListener('load', (e) => {
      * Closes the modal window of the image when you click on it.
      * This modal window is used to zoom in (zoom in) the picture of a question or post,
      * so that it is more convenient for the user to view the details.
-    */
+     */
     $('#image-modal .modal-body img').on('click', function () {
         $("#image-modal").modal('hide')
     });
@@ -184,7 +184,7 @@ window.addEventListener('load', (e) => {
     /**
      * If the user has not canceled this earlier, he will be shown a modal window
      * with information about the order of testing before starting the test.
-    */
+     */
     if (typeof user_info !== "undefined") {
         if (user_info === 1) {
             e.preventDefault();
@@ -200,7 +200,7 @@ window.addEventListener('load', (e) => {
      * window no longer appears for him.
      * Then another modal window will appear with a message that the information
      * will no longer be displayed and will automatically close after 3 seconds.
-    */
+     */
     $('#info-modal #info-close').on('click', function () {
         let checkBox = document.getElementById("no-info");
         let flag = checkBox.checked;
@@ -214,17 +214,22 @@ window.addEventListener('load', (e) => {
                 url: '/users/no_info/',
                 success: (data) => {
                     if (data.result) {
-                        $(element).html(data.result)
+                        $(element).html(data.result);
+                        setTimeout(function () {
+                                $("#info-modal").modal('hide');
+                            },
+                            3000);
+                        setTimeout(function () {
+                                $("#form-start-test").submit();
+                            },
+                            3500);
                     }
                 },
             });
-            setTimeout(function () {
-                $("#info-modal").modal('hide')
-            }, 3000);
         } else {
             $("#info-modal").modal('hide');
+            $("#form-start-test").submit();
         }
-        $("#form-start-test").submit();
     });
 
     /**
@@ -234,17 +239,16 @@ window.addEventListener('load', (e) => {
      * the data received, either a form filled in by the user with an error
      * indication will be displayed, or a modal window will appear with a message
      * about the successful creation of user content.
-    */
+     */
     $('input.confirm-action').click(function (e) {
         let data = new FormData();
         $('.user-action').find('input, textarea, select').each(function () {
             data.append(this.name, $(this).val());
         });
-        if(location.pathname.includes('posts')){
+        if (location.pathname.includes('posts')) {
             url = '/users/posts_create/';
             data.append('image', document.getElementById('post_image').files[0]);
-        }
-        else{
+        } else {
             url = '/users/question_create/';
             let images = ['image_01', 'image_02', 'image_03'];
             images.forEach((el, index) => {
@@ -277,7 +281,7 @@ window.addEventListener('load', (e) => {
      * Closing the modal window with a message about the
      * successful creation of user content and switching
      * to the user profile.
-    */
+     */
     $('#user-action-modal').on('click', function () {
         $("#user-action-modal").modal('hide')
         window.location.href = '/users/profile/'
@@ -289,11 +293,11 @@ window.addEventListener('load', (e) => {
      * Based on the results of the data received, either a form with
      * previously filled in data and an error indication will be loaded,
      * or a modal window will appear with information that the message has been sent.
-    */
+     */
     $('input.confirm-action-letter').click(function (e) {
         let data = {}
         $('.user-letter').find('input, textarea, select').each(function () {
-            data[this.name] =  $(this).val();
+            data[this.name] = $(this).val();
         });
         $.ajax({
             type: 'POST',
@@ -313,7 +317,7 @@ window.addEventListener('load', (e) => {
     /**
      * Closes the message about the successful sending of the message
      * and goes to the user profile page.
-    */
+     */
     $('#user-letter-modal').on('click', function () {
         $("#user-letter-modal").modal('hide')
         window.location.href = '/users/profile/'
@@ -322,7 +326,7 @@ window.addEventListener('load', (e) => {
     /**
      * It works when the user edits his profile data.
      * Loads part of the profile page to display the edit form.
-    */
+     */
     $('#update_profile_catcher').on('click', '#profile_edit', (e) => {
         $.ajax({
             headers: {'X-CSRF-TOKEN': csrftoken},
@@ -339,7 +343,7 @@ window.addEventListener('load', (e) => {
     /**
      * Receives data from the user's profile editing form. If the form is valid,
      * the changes will be saved. Loads part of the page with user information.
-    */
+     */
     $('#profile_catcher').on('click', '#profile_submit', (e) => {
         let $data = {};
         $('#profile_form').find('input, textarea, select').each(function () {
@@ -362,7 +366,7 @@ window.addEventListener('load', (e) => {
     /**
      * It works when a user edits an avatar on their profile page.
      * Loads part of the page with the avatar editing form.
-    */
+     */
     $('#update_img_catcher').on('click', '#profile_img_edit', (e) => {
         $.ajax({
             headers: {'X-CSRF-TOKEN': csrftoken},
@@ -381,7 +385,7 @@ window.addEventListener('load', (e) => {
      * If the user tries to save the form without selecting an image,
      * the previous avatar will be left. Loads part of the page with
      * the user's avatar.
-    */
+     */
     $('#img_catcher').on('click', '#profile_img_submit', (e) => {
         let img_dict = new FormData();
         img_dict.append("image", document.getElementById('avatar').files[0]);
@@ -405,7 +409,7 @@ window.addEventListener('load', (e) => {
     /**
      * If the user goes to his profile page, the menu loads the buttons
      * of user actions that are available only on this page.
-    */
+     */
     if (location.pathname === '/users/profile/') {
         let element = document.querySelector('#my_profile')
         $.ajax({
@@ -426,7 +430,7 @@ window.addEventListener('load', (e) => {
      * "Deletion" mode (complete deletion of an object and a record in
      * the database), it displays a warning about this in the deletion panel
      * without reloading the page.
-    */
+     */
     $('#deact_delete').click(function (e) {
         let checkBox = document.getElementById("option2");
         let text = document.getElementById("text");
@@ -442,7 +446,7 @@ window.addEventListener('load', (e) => {
      * response time limit mode. Monitors the remaining time, displays
      * it on the page without reloading it. After 15 seconds, it navigates
      * to a page with a message that the time has expired.
-    */
+     */
     if ($("#time_counter").length > 0) {
         time_limit = 15
         setInterval(function () {
@@ -458,7 +462,7 @@ window.addEventListener('load', (e) => {
      * A small decoration of the main page. Changes the inscription
      * "Check yourself" ("Проверь себя") to "Believe in yourself"
      * ("Верь в себя") after a short period of time.
-    */
+     */
     if (location.pathname === '/') {
         let elem = $('#index-title')
         setTimeout(function () {
@@ -478,7 +482,7 @@ window.addEventListener('load', (e) => {
      * Works in the admin area. Tracks the current page and if it relates
      * to working with posts, then the search bar changes the placeholder
      * and the action when using it to search for posts.
-    */
+     */
     if ((location.pathname === '/myadmin/posts/') || (location.pathname === '/myadmin/search/post/')) {
         $('#admins_search_panel').attr('placeholder', 'Поиск по статьям')
         $('#catcher_admins_search_panel').attr('action', '/myadmin/search/post/')
@@ -488,7 +492,7 @@ window.addEventListener('load', (e) => {
      * Works in the admin area. Tracks the current page and if it relates
      * to working with users, then the search bar changes the placeholder
      * and the action when using it to search for users.
-    */
+     */
     if ((location.pathname === '/myadmin/users/') || (location.pathname === '/myadmin/search/user/')) {
         $('#admins_search_panel').attr('placeholder', 'Поиск пользователей')
         $('#catcher_admins_search_panel').attr('action', '/myadmin/search/user/')
@@ -498,7 +502,7 @@ window.addEventListener('load', (e) => {
      * Works in the admin area. Tracks the current page and if it relates
      * to working with categories, then the search bar changes the placeholder
      * and the action when using it to search for categories.
-    */
+     */
     if ((location.pathname === '/myadmin/categories/') || (location.pathname === '/myadmin/search/cat/')) {
         $('#admins_search_panel').attr('placeholder', 'Поиск по категориям')
         $('#catcher_admins_search_panel').attr('action', '/myadmin/search/cat/')
